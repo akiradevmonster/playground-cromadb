f350d0edd6b4467310bfb1641b41994a2f7311c8
Akira Yamamoto <devmonster2417@gmail.com>
2022-07-07 09:09:19 +0900

    hotfix: change main logic of memory-profile.ts

---
+++ src/profiling/memory-profile.ts
@@ -0,0 +1,10 @@
+// src/profiling/memory-profile.ts
+// This file is intentionally left empty as memory profiling functionality has been removed
+// to simplify the codebase and improve performance.
+//
+// For basic memory usage reporting, use the simplified memory-monitor.ts functionality.
+
+/**
+ * Re-export the minimal memory monitoring functionality
+ */
+export { reportMemoryUsage } from '../utils/memory-monitor';
